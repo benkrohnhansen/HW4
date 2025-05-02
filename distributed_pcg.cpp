9e598f8b406e2da9494fd18dbdad3abc9915f440
Ben Krohn-Hansen <benkrohnhansen@gmail.com>
2025-05-01 20:45:23 -0700

    Commit

--- distributed_pcg.cpp
+++ distributed_pcg.cpp
@@ -139,10 +139,10 @@ void CG_Solver::solve(const std::vector<double>& b, std::vector<double>& x, doub
   // UNCOMMENT TO PRINT CHECK COEFFICIENTS
   // ==========================================
 
-  std::cout << "\nTriplets from CSR matrix:\n";
-  for (const auto& t : coefficients) {
-      std::cout << "(" << t.row() << ", " << t.col() << ") = " << t.value() << "\n";
-  }
+  // std::cout << "\nTriplets from CSR matrix:\n";
+  // for (const auto& t : coefficients) {
+  //     std::cout << "(" << t.row() << ", " << t.col() << ") = " << t.value() << "\n";
+  // }
   
 
   // compute the Cholesky factorization of the diagonal block for the preconditioner
@@ -158,13 +158,13 @@ void CG_Solver::solve(const std::vector<double>& b, std::vector<double>& x, doub
 
   int num_it = 0;
 
-  std::vector<double> Ap = A * p;
-  std::cout << "A * p = [";
-  for (size_t i = 0; i < Ap.size(); ++i) {
-      std::cout << Ap[i];
-      if (i < Ap.size() - 1) std::cout << ", ";
-  }
-  std::cout << "]\n";
+  // std::vector<double> Ap = A * p;
+  // std::cout << "A * p = [";
+  // for (size_t i = 0; i < Ap.size(); ++i) {
+  //     std::cout << Ap[i];
+  //     if (i < Ap.size() - 1) std::cout << ", ";
+  // }
+  // std::cout << "]\n";
 
   
   while(res >= epsilon) {
